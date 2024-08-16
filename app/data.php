a17229d90ebb04e8b11185814cfad4d9e34745b2
developerimran <developerimran.in@gmail.com>
2024-08-16 20:36:58 +0600

    All Uplode

---
+++ app/data.php
@@ -0,0 +1,54 @@
+<?php
+
+$top_ber=[
+
+"email"          => "imran.ppi453@gmail.com",
+"phone"          => "01756041512",
+"socialLink"     => [ 
+                        "twitter"       => "https://x.com/",
+                        "facebook"      => "https://www.facebook.com/",
+                        "g+"            => "https://www.google.com/",
+                        "linkedin"      => "https://www.linkedin.com/",
+                        "instagram"     => "https://www.linkedin.com/",
+                    ]
+];
+$headerImage= [
+                "image"         => "https://png.pngtree.com/png-clipart/20221219/original/pngtree-kacchi-biryani-bangla-restaurant-logo-png-image_8775214.png",
+                 "titel"          => "The best restaurant in town - The New York Times",
+  ];
+                
+$Menu=[    
+    "home"                    => "index.php",
+    "menu"                    => "menu.php",
+    "location"                => "Location.php",
+    "blog"                    => "blog.php",
+    "reservation"             => "reservation.php",
+    "staff"                   => "staff.php",
+    "news"                    => "news.php",
+    "gallery"                 => "gallery.php"
+];
+$banner=[
+    [
+        "image"     =>"https://www.foodiesfeed.com/wp-content/uploads/2023/09/healthy-food.jpg",
+        "title"     =>"নান্না বিরিয়ানি খেতে পুরান ঢাকায়",
+        "disc"      =>"১৯৬২ সালের দিকে পুরান ঢাকার মৌলভীবাজারে বাবুর্চি নান্না মিয়া বিরিয়ানির দোকান দিয়ে আলোচনায় আসে। এখানে খাসির কাচ্চির পাশাপাশি মোরগ-পোলাও, খাসির বিরিয়ানি, খাসির রেজালা, ফিন্নি টিকিয়া ও বোরহানি পাওয়া যায়। সেই সঙ্গে তারা বিভিন্ন অনুষ্ঠানের অর্ডার নিয়েও কাজ করে।  ",
+    ],
+    [
+        "image"     =>"https://t4.ftcdn.net/jpg/02/86/17/89/360_F_286178925_8zk89O9uC5JJVPvqhvBMUpaRxp8AFXzD.jpg",
+        "title"     =>"কাচ্চি বিরিয়ানি",
+        "disc"      =>"কাচ্চি বিরিয়ানির কাচ্চি শব্দটি এসেছে উর্দু কাচ্চা শব্দটি থেকে, যার বাংলা অর্থ কাঁচা। যেহেতু সুগন্ধি চালের সাথে মাংস সরাসরি রান্না করা হয়, তাই এর নাম হয়েছে কাচ্চি। এটি হিন্দি এবং উর্দুতেও একই নামে পরিচিত। সেদ্ধ ছাড়া খাসির মাংস টকদই দিয়ে মাখিয়ে তার উপর আলু আর চালের আস্তরণ দিয়ে রান্না করা হয় কাচ্চি বিরিয়ানি।",
+    ],
+    [
+        "image"     =>"https://www.shutterstock.com/image-photo/table-food-top-view-260nw-467823860.jpg",
+        "title"     =>"চিকেন বিরিয়ানি ",
+        "disc"      =>"চিকেন বিরিয়ানি রেসিপি – আমাদের দেশে সবচেয়ে জনপ্রিয় রেসিপিগুলোর মধ্যে একটি হলো বিরিয়ানি। ভোজনরসিক বাঙ্গালীদেরতো বিরিয়ানির নাম শুনলেই জিভে জল চলে আসে। এখনতো বিভিন্ন উৎসব ও অনুষ্ঠানে বিরিয়ানির চল বেশ জনপ্রিয় হয়ে উঠেছে। তবে চলুন রেস্টুরেন্ট নয়,ঘরে বসে বাড়িতেই বানিয়ে নেওয়া যাক স্পেশাল চিকেন বিরিয়ানি রেসিপিটি। দেখবেন বাইরের স্বাদ একেবারে ভুলে গেছেন।",
+    ],
+
+];
+
+            
+
+?>
+
+
+
